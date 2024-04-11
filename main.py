7793dee83fc39755ec655f8f01b2689b1901ebdf
Arbuzzopnik <116891788+Arbuzzopnik@users.noreply.github.com>
2024-04-11 05:13:51 +0300

    добавил разворот через докер

--- main.py
+++ main.py
@@ -1,39 +1,32 @@
-import socket
-import time
-
 import paramiko
 
-
-def main():
-    pass
 
 
 def connect(ip_addr: str, username: str, commands: tuple):
     client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    client.connect(
-        hostname=ip_addr,
-        username=username,
-    )
-    with client.invoke_shell() as ssh:
-        result = {}
-        for command in commands:
-            ssh.send(f"{command}\n")
-            ssh.settimeout(5)
-            output = ""
-            while True:
-                try:
-                    part = ssh.recv(60000).decode("utf-8")
-                    output += part
-                    time.sleep(0.5)
-                except socket.timeout:
-                    break
-                result[command] = output
-        return result
+    client.connect(hostname=ip_addr, username=username)
 
+    result = {}
+    for command in commands:
+        stdin, stdout, stderr = client.exec_command(command)
+        output = stdout.read().decode('utf-8')
+        print(output)
+        result[command] = output
+
+    client.close()
+    return result
 
 
 if __name__ == '__main__':
-    commands = ("pwd", "ls")
-    result = connect(ip_addr="192.168.56.102", username="arbuzzopnik", password="A052000", commands=commands)
-    print(result["pwd"])
+    commands = ("mkdir -p $HOME/docker/volumes/postgres",
+                "docker pull postgres",
+                "docker run --rm --name postgres_test -e POSTGRES_PASSWORD=pass -e "
+                "POSTGRES_USER=admin -e POSTGRES_DB=postgres_test_db -d -p 5432:5432 -v "
+                "$HOME/docker/volumes/postgres:/var/lib/postgresql/data postgres",
+                "docker exec -it postgres_test psql -U admin -d postgres_test_db -c 'SELECT 1;'"
+                )
+    commands_2 = ("docker exec postgres_test psql -U admin -d postgres_test_db -c 'SELECT 1;'",)
+    result = connect(ip_addr="192.168.56.102", username="arbuzzopnik", commands=commands)
+    for key, value in result.items():
+        print(f'{key}: {value}')
