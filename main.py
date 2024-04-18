71ef43ed60f81674144c2f11626a7c15357a9bf9
Arbuzzopnik <116891788+Arbuzzopnik@users.noreply.github.com>
2024-04-18 13:13:13 +0300

    добавлены ssh_connect и request

--- main.py
+++ main.py
@@ -1,49 +1,73 @@
 import paramiko
+from tqdm import tqdm
 
 
-def connect(ip_addr: str, username: str, commands: tuple):
+def ssh_connect(ip_addr: str, username: str):
     client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     client.connect(hostname=ip_addr, username=username)
+    return client
 
-    result = {}
-    for command in commands:
-        stdin, stdout, stderr = client.exec_command(command)
-        output = stdout.read().decode('utf-8')
-        # print(output)
-        result[command] = output
+
+def connect(ip_addr: str, username: str, commands: dict):
+    client = ssh_connect(ip_addr, username)
+    for key, value in tqdm(commands.items(), colour=False):
+        stdin, stdout, stderr = client.exec_command(value)
+
+        tqdm.write(f"{key}", end='')
+
         error = stderr.read().decode('utf-8')
 
         if error:
-            print(f"Error occurred while running command '{command}': {error}")
-            result[command] = error
+            tqdm.write("\033[91m\u2717\033[0m")  # Красный крестик
+            print(f"\nError occurred while running command '{value}': {error}")
+            break
         else:
-            #print(output)
-            result[command] = output
-
+            tqdm.write("\033[92m\u2713\033[0m")  # Зеленая галочка
     client.close()
-    return result
+
+
+def request(ip_addr: str, username: str, db_config: dict, request_text: str):
+    client = ssh_connect(ip_addr, username)
+    stdin, stdout, stderr = client.exec_command(
+        f"docker exec {db_config['container_name']} psql -U {db_config['user']} -d {db_config['db_name']} -c '{request_text}'")
+
+    output = stdout.read().decode('utf-8')
+    error = stderr.read().decode('utf-8')
+
+    if error:
+        print(error)
+    else:
+        print(output)
+    client.close()
+
+
+def main():
+    ip_addr = "192.168.56.103"
+    username = "admin"
+    request_text = "SELECT 1;"
+    db_config = {'container_name': 'postgres_test',
+                 'db_name': 'postgres_test_db',
+                 'user': 'admin',
+                 'password': 'pass',
+                 }
+
+    commands = {'Создание директории для монтирования': 'mkdir -p $HOME/docker/volumes/postgres',
+                'Загрузка образа PostgreSQL из Docker Hub': "docker pull postgres",
+                'Запуск контейнер PostgreSQL': f"docker run -d --restart unless-stopped --name {db_config['container_name']} -e " 
+                                               f"POSTGRES_PASSWORD={db_config['password']} -e "
+                                               f"POSTGRES_USER={db_config['username']} -e POSTGRES_DB={db_config['db_name']} -d -p 5432:5432 -v "
+                                               "$HOME/docker/volumes/postgres:/var/lib/postgresql/data postgres",
+                }
+    commands_to_dell = {'1': f'docker stop {db_config["container_name"]}',
+                        '2': f'docker rm {db_config["container_name"]}',
+                        '3': f'docker rmi postgres',
+                        '4': f'rm - r $HOME/docker/volumes/postgres',
+                        }
+
+    connect(ip_addr=ip_addr, username=username, commands=commands)
+    request(ip_addr=ip_addr, username=username, db_config=db_config, request_text=request_text)
 
 
 if __name__ == '__main__':
-    commands = ("mkdir -p $HOME/docker/volumes/postgres",
-                "docker pull postgres",
-                "docker run -d --restart unless-stopped --name postgres_test -e POSTGRES_PASSWORD=pass -e "
-                "POSTGRES_USER=admin -e POSTGRES_DB=postgres_test_db -d -p 5432:5432 -v "
-                "$HOME/docker/volumes/postgres:/var/lib/postgresql/data postgres",
-                "docker exec postgres_test psql -U admin -d postgres_test_db -c 'SELECT 1;'"
-                )
-    commands_to_dell = ('docker stop postgres_test',
-                        'docker rm postgres_test',
-                        'docker rmi postgres',
-                        'rm - rf $HOME/docker/volumes/postgres',
-                        )
-    commands_2 = ("docker exec postgres_test psql -U admin -d postgres_test_db -c 'SELECT 1;'",)
-    commands_3 = ("pwd", "ls",)
-
-    result = connect(ip_addr="192.168.56.103", username="admin", commands=commands)
-    for key, value in result.items():
-        print(f'{key}: {value}')
-    result = connect(ip_addr="192.168.56.103", username="admin", commands=commands_2)
-    for key, value in result.items():
-        print(f'{key}: {value}')
+    main()
